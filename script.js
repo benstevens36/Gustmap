6bdc1c37c137109706c399702da1a13b4d00d09e
Ben Stevens <78230020+benstevens36@users.noreply.github.com>
2023-12-07 16:05:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,87 +1,87 @@
+let video, canvasOutput, canvasContext, cap, frame1, prvs, next, hsv, flow, mag, ang, hsv0, hsv1, hsv2, hsvVec;
+
+function onOpenCVReady() {
+    video = document.getElementById("cameraFeed");
+    canvasOutput = document.getElementById("canvasOutput");
+    canvasContext = canvasOutput.getContext("2d");
+
+    cap = new cv.VideoCapture(video);
+    frame1 = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+    prvs = new cv.Mat(video.height, video.width, cv.CV_8UC1);
+    hsv = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+    flow = new cv.Mat(video.height, video.width, cv.CV_32FC2);
+    mag = new cv.Mat(video.height, video.width, cv.CV_32FC1);
+    ang = new cv.Mat(video.height, video.width, cv.CV_32FC1);
+    hsv0 = new cv.Mat(video.height, video.width, cv.CV_8UC1);
+    hsv1 = new cv.Mat(video.height, video.width, cv.CV_8UC1, new cv.Scalar(255));
+    hsv2 = new cv.Mat(video.height, video.width, cv.CV_8UC1);
+    hsvVec = new cv.MatVector();
+    hsvVec.push_back(hsv0); hsvVec.push_back(hsv1); hsvVec.push_back(hsv2);
+    next = new cv.Mat(video.height, video.width, cv.CV_8UC1);
+
+    startCamera();
+}
+
+function checkOpenCvReady() {
+    if (cv && cv.imread) {
+        onOpenCVReady();
+    } else {
+        console.log("Waiting for OpenCV to be ready...");
+        setTimeout(checkOpenCvReady, 50);
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
-    let video = document.getElementById("cameraFeed");
-    let canvasOutput = document.getElementById("canvasOutput");
-    let canvasContext = canvasOutput.getContext("2d");
+    checkOpenCvReady();
+});
 
-    let cap, frame1, prvs, next;
+function startCamera() {
+    navigator.mediaDevices.getUserMedia({ video: true })
+        .then(function (stream) {
+            video.srcObject = stream;
+            video.onloadedmetadata = function (e) {
+                video.play();
+                cap.read(frame1);
+                cv.cvtColor(frame1, prvs, cv.COLOR_RGBA2GRAY);
+                frame1.delete();
+                requestAnimationFrame(processVideo);
+            };
+        })
+        .catch(function (err) {
+            console.error("An error occurred: " + err);
+        });
+}
 
-    function onOpenCVReady() {
-        cap = new cv.VideoCapture(video);
-        frame1 = new cv.Mat(video.height, video.width, cv.CV_8UC4);
-        prvs = new cv.Mat();
-        next = new cv.Mat();
-        startCamera();
-    }
-
-    function checkOpenCVInitialization() {
-        if (cv && cv.imread) {
-            onOpenCVReady();
-        } else {
-            setTimeout(checkOpenCVInitialization, 50);
-        }
-    }
-
-    setTimeout(checkOpenCVInitialization, 50);
-
-    function startCamera() {
-        navigator.mediaDevices.getUserMedia({ video: true })
-            .then(function (stream) {
-                video.srcObject = stream;
-                video.onloadedmetadata = function (e) {
-                    video.play();
-                    cap.read(frame1);
-                    cv.cvtColor(frame1, prvs, cv.COLOR_RGBA2GRAY);
-                    frame1.delete();
-                    requestAnimationFrame(processVideo);
-                };
-            })
-            .catch(function (err) {
-                console.error("An error occurred while accessing the camera: " + err);
-            });
-    }
-
-    function drawArrow(context, fromX, fromY, toX, toY, color) {
-        var headLength = 10; // length of head in pixels
-        var dx = toX - fromX;
-        var dy = toY - fromY;
-        var angle = Math.atan2(dy, dx);
-        context.strokeStyle = color;
-        context.lineWidth = 2;
-        context.beginPath();
-        context.moveTo(fromX, fromY);
-        context.lineTo(toX, toY);
-        context.lineTo(toX - headLength * Math.cos(angle - Math.PI / 6), toY - headLength * Math.sin(angle - Math.PI / 6));
-        context.moveTo(toX, toY);
-        context.lineTo(toX - headLength * Math.cos(angle + Math.PI / 6), toY - headLength * Math.sin(angle + Math.PI / 6));
-        context.stroke();
-    }
-
-    function processVideo() {
+function processVideo() {
+    try {
         let frame2 = new cv.Mat(video.height, video.width, cv.CV_8UC4);
         cap.read(frame2);
         cv.cvtColor(frame2, next, cv.COLOR_RGBA2GRAY);
 
-        let flow = new cv.Mat();
         cv.calcOpticalFlowFarneback(prvs, next, flow, 0.5, 3, 15, 3, 5, 1.2, 0);
+        cv.split(flow, hsvVec);
+        let u = hsvVec.get(0);
+        let v = hsvVec.get(1);
+        cv.cartToPolar(u, v, mag, ang);
+        u.delete(); v.delete();
+        
+        ang.convertTo(hsv0, cv.CV_8UC1, 180 / Math.PI / 2);
+        cv.normalize(mag, hsv2, 0, 255, cv.NORM_MINMAX, cv.CV_8UC1);
+        
+        hsvVec = new cv.MatVector();
+        hsvVec.push_back(hsv0); 
+        hsvVec.push_back(hsv1); 
+        hsvVec.push_back(hsv2);
 
-        canvasContext.clearRect(0, 0, video.width, video.height);
-        canvasContext.drawImage(video, 0, 0, video.width, video.height);
+        cv.merge(hsvVec, hsv);
+        cv.cvtColor(hsv, frame2, cv.COLOR_HSV2RGB);
 
-        // Increase the spacing to reduce the number of arrows
-        for (let y = 0; y < video.height; y += 40) {
-            for (let x = 0; x < video.width; x += 40) {
-                let idx = (y * video.width + x) * 2;
-                let u = flow.data32F[idx];
-                let v = flow.data32F[idx + 1];
-                if (u > 2 || v > 2) {
-                    drawArrow(canvasContext, x, y, x + u, y + v, 'red');
-                }
-            }
-        }
+        cv.imshow('canvasOutput', frame2);
 
         next.copyTo(prvs);
         frame2.delete();
-        flow.delete();
         requestAnimationFrame(processVideo);
+    } catch (err) {
+        console.error("Error in processVideo: ", err);
     }
-});
+}
