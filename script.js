ddb31b1143cfa0ef2433c793be9038b25098d7eb
Ben Stevens <78230020+benstevens36@users.noreply.github.com>
2023-12-07 16:31:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -56,20 +56,50 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function processVideo() {
     try {
-        console.log("Processing video frame...");
-        let frame1 = new cv.Mat(video.height, video.width, cv.CV_8UC4);
-        let dst = new cv.Mat(video.height, video.width, cv.CV_8UC1); // Destination image
+        let frame2 = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+        cap.read(frame2);
+        let next = new cv.Mat(video.height, video.width, cv.CV_8UC1);
+        cv.cvtColor(frame2, next, cv.COLOR_RGBA2GRAY);
 
-        cap.read(frame1); // Read a frame from the video
-        cv.cvtColor(frame1, dst, cv.COLOR_RGBA2GRAY); // Convert to grayscale
+        let flow = new cv.Mat();
+        cv.calcOpticalFlowFarneback(prvs, next, flow, 0.5, 3, 15, 3, 5, 1.2, 0);
 
-        cv.imshow('canvasOutput', dst); // Display the processed frame on the canvas
+        canvasContext.clearRect(0, 0, video.width, video.height);
 
-        frame1.delete(); // Clean up
-        dst.delete();
+        // Draw arrows to represent the optical flow
+        for (let y = 0; y < video.height; y += 20) {
+            for (let x = 0; x < video.width; x += 20) {
+                let idx = (y * video.width + x) * 2;
+                let u = flow.data32F[idx];
+                let v = flow.data32F[idx + 1];
+                if (Math.abs(u) > 2 || Math.abs(v) > 2) { // Threshold to filter small movements
+                    drawArrow(canvasContext, x, y, x + u, y + v);
+                }
+            }
+        }
 
-        requestAnimationFrame(processVideo); // Continue processing frames
+        prvs.delete();
+        prvs = next;
+        frame2.delete();
+        flow.delete();
+        requestAnimationFrame(processVideo);
     } catch (err) {
         console.error("Error processing video frame: ", err);
     }
+}
+
+function drawArrow(context, fromX, fromY, toX, toY) {
+    var headLength = 10; // length of head in pixels
+    var dx = toX - fromX;
+    var dy = toY - fromY;
+    var angle = Math.atan2(dy, dx);
+    context.strokeStyle = 'red';
+    context.lineWidth = 2;
+    context.beginPath();
+    context.moveTo(fromX, fromY);
+    context.lineTo(toX, toY);
+    context.lineTo(toX - headLength * Math.cos(angle - Math.PI / 6), toY - headLength * Math.sin(angle - Math.PI / 6));
+    context.moveTo(toX, toY);
+    context.lineTo(toX - headLength * Math.cos(angle + Math.PI / 6), toY - headLength * Math.sin(angle + Math.PI / 6));
+    context.stroke();
 }
