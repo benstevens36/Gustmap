533f36e3901584b379ca2121f4145289a3188986
Ben Stevens <78230020+benstevens36@users.noreply.github.com>
2023-12-07 16:12:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,53 @@
+console.log("Script loaded. Setting up OpenCV check...");
+
 let video, canvasOutput, canvasContext, cap, frame1, prvs, next, hsv, flow, mag, ang, hsv0, hsv1, hsv2, hsvVec;
 
 function onOpenCVReady() {
+    console.log("OpenCV.js is ready.");
+
     video = document.getElementById("cameraFeed");
     canvasOutput = document.getElementById("canvasOutput");
     canvasContext = canvasOutput.getContext("2d");
 
-    cap = new cv.VideoCapture(video);
-    frame1 = new cv.Mat(video.height, video.width, cv.CV_8UC4);
-    prvs = new cv.Mat(video.height, video.width, cv.CV_8UC1);
-    hsv = new cv.Mat(video.height, video.width, cv.CV_8UC4);
-    flow = new cv.Mat(video.height, video.width, cv.CV_32FC2);
-    mag = new cv.Mat(video.height, video.width, cv.CV_32FC1);
-    ang = new cv.Mat(video.height, video.width, cv.CV_32FC1);
-    hsv0 = new cv.Mat(video.height, video.width, cv.CV_8UC1);
-    hsv1 = new cv.Mat(video.height, video.width, cv.CV_8UC1, new cv.Scalar(255));
-    hsv2 = new cv.Mat(video.height, video.width, cv.CV_8UC1);
-    hsvVec = new cv.MatVector();
-    hsvVec.push_back(hsv0); hsvVec.push_back(hsv1); hsvVec.push_back(hsv2);
-    next = new cv.Mat(video.height, video.width, cv.CV_8UC1);
+    if (!video || !canvasOutput) {
+        console.error("HTML elements not found.");
+        return;
+    }
 
-    startCamera();
+    try {
+        cap = new cv.VideoCapture(video);
+        frame1 = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+        prvs = new cv.Mat(video.height, video.width, cv.CV_8UC1);
+        next = new cv.Mat(video.height, video.width, cv.CV_8UC1);
+        startCamera();
+    } catch (error) {
+        console.error("Error during OpenCV objects initialization: ", error);
+    }
 }
 
 function checkOpenCvReady() {
     if (cv && cv.imread) {
+        console.log("Checking if OpenCV is ready...");
         onOpenCVReady();
     } else {
-        console.log("Waiting for OpenCV to be ready...");
+        console.log("Waiting for OpenCV.js to be ready...");
         setTimeout(checkOpenCvReady, 50);
     }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
+    console.log("DOM fully loaded and parsed.");
     checkOpenCvReady();
 });
 
 function startCamera() {
+    console.log("Attempting to start camera...");
     navigator.mediaDevices.getUserMedia({ video: true })
         .then(function (stream) {
+            console.log("Camera feed started.");
             video.srcObject = stream;
             video.onloadedmetadata = function (e) {
+                console.log("Camera metadata loaded, starting video...");
                 video.play();
                 cap.read(frame1);
                 cv.cvtColor(frame1, prvs, cv.COLOR_RGBA2GRAY);
@@ -48,40 +56,24 @@ function startCamera() {
             };
         })
         .catch(function (err) {
-            console.error("An error occurred: " + err);
+            console.error("An error occurred while accessing the camera: ", err);
         });
 }
 
 function processVideo() {
     try {
+        console.log("Processing video frame...");
         let frame2 = new cv.Mat(video.height, video.width, cv.CV_8UC4);
         cap.read(frame2);
         cv.cvtColor(frame2, next, cv.COLOR_RGBA2GRAY);
 
-        cv.calcOpticalFlowFarneback(prvs, next, flow, 0.5, 3, 15, 3, 5, 1.2, 0);
-        cv.split(flow, hsvVec);
-        let u = hsvVec.get(0);
-        let v = hsvVec.get(1);
-        cv.cartToPolar(u, v, mag, ang);
-        u.delete(); v.delete();
-        
-        ang.convertTo(hsv0, cv.CV_8UC1, 180 / Math.PI / 2);
-        cv.normalize(mag, hsv2, 0, 255, cv.NORM_MINMAX, cv.CV_8UC1);
-        
-        hsvVec = new cv.MatVector();
-        hsvVec.push_back(hsv0); 
-        hsvVec.push_back(hsv1); 
-        hsvVec.push_back(hsv2);
-
-        cv.merge(hsvVec, hsv);
-        cv.cvtColor(hsv, frame2, cv.COLOR_HSV2RGB);
-
-        cv.imshow('canvasOutput', frame2);
+        // Initialize additional OpenCV objects here
+        // ...
 
         next.copyTo(prvs);
         frame2.delete();
         requestAnimationFrame(processVideo);
     } catch (err) {
-        console.error("Error in processVideo: ", err);
+        console.error("Error processing video frame: ", err);
     }
 }
